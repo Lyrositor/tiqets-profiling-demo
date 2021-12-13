31a458652d3505cf4c19bbd751b9930bc6730628
Marc Gagné <marc.gagne@tiqets.com>
2021-12-13 17:20:37 +0100

    Update with more accurate numbers
    yappi profiling was distorting the stats.

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ A quick demo of the value of profiling and making simple optimizations to your c
 
 This is not meant to be rigorous by any stretch of the imagination. The numbers are merely illustrative.
 
-Benchmark times for a single run on my laptop:
+Benchmark times for a single run on my laptop (without yappi profiling, as this creates distorted numbers due to the overhead from profiling):
 
 | benchmark                          | time       |
 |------------------------------------|------------|
-| example_0 (baseline)               | 54.890144s |
-| example_1 (less magic)             | 15.408645s |
-| example_2 (better data structures) | 3.822141s  |
-| example_3 (more direct access)     | 2.08018s   |
-| example_4 (fewer loops)            | 1.77723s   |
-| example_5 (no dataclasses)         | 1.770482s  |
-| example_6 (Cython)                 | 0.516549s  |
-| example_7 (typed Cython)           | 0.416739s  |
+| example_0 (baseline)               | 92.600269s |
+| example_1 (less magic)             | 47.535969s |
+| example_2 (better data structures) | 10.17423s  |
+| example_3 (more direct access)     | 8.414011s  |
+| example_4 (fewer loops)            | 7.316188s  |
+| example_5 (no dataclasses)         | 5.798112s  |
+| example_6 (Cython)                 | 5.512598s  |
+| example_7 (typed Cython)           | 2.874036s  |
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from decimal import Decimal
 from importlib import import_module
 from typing import Type, Any
@@ -11,50 +12,56 @@ STATUS_INACTIVE = 2
 
 EXAMPLES = ["example_0", "example_1", "example_2", "example_3", "example_4", "example_5", "example_6", "example_7"]
 
+WITH_YAPPI_PROFILING = False
+
 
 def run_benchmarks() -> None:
     print("Running benchmarks")
     for example in EXAMPLES:
         print(f"===== {example} =====")
-        example_duration = run_benchmark(example)
+        example_duration = profile_benchmark(example)
         print(f"{example}: {example_duration.total_seconds()}s")
     print("Done")
 
 
-def run_benchmark(benchmark_name: str) -> pendulum.Duration:
+def profile_benchmark(benchmark_name: str) -> pendulum.Duration:
     benchmark_module: Any = import_module(benchmark_name)
     start = pendulum.now()
-    with yappi_profile(f"profile_{benchmark_name}"):
-        root_1 = setup_entities(
-            benchmark_module.PPCEntity,
-            i_cpc=Decimal("0.01"),
-            j_cpc=Decimal("0.02"),
-            k_cpc=Decimal("0.03"),
-            i_status=STATUS_ACTIVE,
-            j_status=STATUS_INACTIVE,
-            k_status=STATUS_INACTIVE
-        )
-        root_2 = setup_entities(
-            benchmark_module.PPCEntity,
-            i_cpc=Decimal("0.04"),
-            j_cpc=Decimal("0.09"),
-            k_cpc=Decimal("0.02"),
-            i_status=STATUS_ACTIVE,
-            j_status=STATUS_ACTIVE,
-            k_status=STATUS_ACTIVE
-        )
-        diff = benchmark_module.make_diff(root_1, root_2)
-        with open("benchmark_name_diff.txt", "w") as f:
-            benchmark_module.write_diff(f, diff)
+    with yappi_profile(f"profile_{benchmark_name}") if WITH_YAPPI_PROFILING else nullcontext():
+        run_benchmark(benchmark_module)
     end = pendulum.now()
     return end - start
+
+
+def run_benchmark(benchmark_module: Any) -> None:
+    root_1 = setup_entities(
+        benchmark_module.PPCEntity,
+        i_cpc=Decimal("0.01"),
+        j_cpc=Decimal("0.02"),
+        k_cpc=Decimal("0.03"),
+        i_status=STATUS_ACTIVE,
+        j_status=STATUS_INACTIVE,
+        k_status=STATUS_INACTIVE
+    )
+    root_2 = setup_entities(
+        benchmark_module.PPCEntity,
+        i_cpc=Decimal("0.04"),
+        j_cpc=Decimal("0.09"),
+        k_cpc=Decimal("0.02"),
+        i_status=STATUS_ACTIVE,
+        j_status=STATUS_ACTIVE,
+        k_status=STATUS_ACTIVE
+    )
+    diff = benchmark_module.make_diff(root_1, root_2)
+    with open("benchmark_diff.txt", "w") as f:
+        benchmark_module.write_diff(f, diff)
 
 
 def setup_entities(
     entity_cls: Type, i_cpc: Decimal, j_cpc: Decimal, k_cpc: Decimal, i_status: int, j_status: int, k_status: int
 ) -> Any:
     root = entity_cls(name="root", cpc=Decimal("0.05"), status=STATUS_ACTIVE)
-    for i in range(10):
+    for i in range(100):
         child_i = entity_cls(name=f"child_i_{i}", cpc=i_cpc, status=i_status)
         root.add_child(child_i)
         for j in range(100):
